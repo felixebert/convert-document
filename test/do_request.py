ea5c9d3cfb0cf43eedc17885d1c9f4f29c4214f8
Felix Ebert <felix.ebert@if-core.de>
2020-02-24 17:10:16 +0100

    local docker deployment, do_request testcase

---
+++ test/do_request.py
@@ -0,0 +1,30 @@
+import requests
+import traceback
+from requests import RequestException, HTTPError
+
+import os
+
+files = os.listdir("defect/")
+
+def do_request(file_path, file_name):
+    fh = open(file_path, 'rb')
+    print("convert " + file_name)
+    try:
+        files = {'file': (file_name, fh, "application/msword")}
+        res = requests.post("http://localhost:3000/convert",
+                            files=files,
+                            timeout=(5, 305),
+                            stream=True)
+        res.raise_for_status()
+        print("OK")
+    except RequestException as exc:
+        print("Conversion failed")
+        traceback.print_tb(exc.__traceback__)
+    finally:
+        fh.close()
+
+
+while True:
+    for file in files:
+        if file.endswith(".doc"):
+            do_request("defect/" + file, file)
